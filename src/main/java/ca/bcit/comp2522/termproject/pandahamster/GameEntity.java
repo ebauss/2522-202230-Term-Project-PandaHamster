a9cd018ca5caba84d0d6c55c9d97cee2c1dbd622
Evon Bausa <evon.bausa@gmail.com>
2022-11-17 14:57:27 -0800

    Append empty GameEntity Interface

---
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/GameEntity.java
@@ -0,0 +1,10 @@
+package ca.bcit.comp2522.termproject.pandahamster;
+
+/**
+ * Represents an interface of type GameEntity.
+ *
+ * @author Evon Bausa & Alex Liu
+ * @version 0.0
+ */
+public interface GameEntity {
+}
