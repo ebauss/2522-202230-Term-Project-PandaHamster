6fc343ba9d0dca37195ed2f5d584406d0b0de458
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 15:41:46 -0800

    iterate and get each tiled tile layer

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
@@ -30,6 +30,12 @@ public final class MapRenderer {
             // TiledTileLayers are the layers where actual tiles are drawn
             if (tiledLayer instanceof TiledTileLayer) {
                 TiledTileLayer tiledTileLayer = (TiledTileLayer) tiledLayer;
+                // iterate through all the tiles on the layer
+                for (int x = 0; x < map.getWidth(); x++) {
+                    for (int y = 0; y < map.getHeight(); y++) {
+
+                    }
+                }
             }
         }
     }
