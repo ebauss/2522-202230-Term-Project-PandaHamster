00183e27bc85920fcfce4cf03dee540144e2f8bc
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 15:36:00 -0800

    start on MapRenderer

---
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
@@ -0,0 +1,23 @@
+package ca.bcit.comp2522.termproject.pandahamster;
+
+import javafx.scene.layout.GridPane;
+import org.tiledreader.TiledMap;
+
+/**
+ * Class to render a tmx file (a tiled map).
+ * @author Alex Liu
+ * @version 2022
+ */
+public final class MapRenderer {
+    private MapRenderer() { }
+
+    /**
+     * Renders a map into a StackPane of GridPane's. Each tile will be stored
+     * as an ImageView in each cell of the GridPane.
+     * @param map the map to render
+     * @return the rendered map
+     */
+    public GridPane render(final TiledMap map) {
+
+    }
+}
