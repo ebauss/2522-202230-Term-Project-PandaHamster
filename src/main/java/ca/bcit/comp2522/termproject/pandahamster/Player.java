e3f482735349570bfb204b2da52a274ddf1de4b2
Evon Bausa <evon.bausa@gmail.com>
2022-11-17 14:35:05 -0800

    Append instance variables for Player class

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/Player.java
@@ -1,4 +1,18 @@
 package ca.bcit.comp2522.termproject.pandahamster;
 
+import java.util.ArrayList;
+
+/**
+ * Represents an object of type Player.
+ *
+ * @author Evon Bausa & Alex Liu
+ * @version 0.0
+ */
 public class Player {
+    private long maxLevel;
+    private long level;
+    private long currentExp;
+    private long money;
+    private ArrayList<AbstractWeapon> weaponInventory;
+    private short lifeCount;
 }
