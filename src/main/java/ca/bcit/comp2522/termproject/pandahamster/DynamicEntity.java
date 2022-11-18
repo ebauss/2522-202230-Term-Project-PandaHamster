d5f3038e09ddf0635eb5583fa7bb025055eb8013
Evon Bausa <evon.bausa@gmail.com>
2022-11-17 16:58:23 -0800

    Append DynamicEntity

---
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/DynamicEntity.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.pandahamster;
+
+/**
+ * Represents an interface of type DynamicEntity.
+ *
+ * @author Evon Bausa & Alex Liu
+ * @version 0.0
+ */
+public interface DynamicEntity {
+    /**
+     * Moves the object on the map.
+     */
+    void move();
+}
