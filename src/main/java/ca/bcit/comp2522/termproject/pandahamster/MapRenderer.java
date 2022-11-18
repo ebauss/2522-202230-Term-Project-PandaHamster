0eee2e0369252b75f18cff71df2c0aaf4503a2c2
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 20:39:22 -0800

    add the grid pane to the stack pane

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
@@ -47,6 +47,7 @@ public final class MapRenderer {
                         gridPane.add(imageView, x, y);
                     }
                 }
+                stackPane.getChildren().add(gridPane);
             }
         }
         return stackPane;
