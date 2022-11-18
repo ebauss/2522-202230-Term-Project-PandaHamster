cbcccdbe9e528c05197ff62a5c393bd6d06e3a90
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 20:00:30 -0800

    initialize the hashmap, crop image

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/MapRenderer.java
@@ -3,9 +3,7 @@ package ca.bcit.comp2522.termproject.pandahamster;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import org.tiledreader.TiledLayer;
-import org.tiledreader.TiledMap;
-import org.tiledreader.TiledTileLayer;
+import org.tiledreader.*;
 
 import java.util.HashMap;
 
@@ -24,7 +22,11 @@ public final class MapRenderer {
      * @return the rendered map
      */
     public StackPane render(final TiledMap map) {
+        StackPane stackPane = new StackPane();
         HashMap<String, HashMap<Integer, Image>> tiles = new HashMap<>();
+        for (TiledTileset tiledTileset: map.getTilesets()) {
+            tiles.put(tiledTileset.getName(), new HashMap<>());
+        }
         // iterate through each layer
         for (TiledLayer tiledLayer: map.getTopLevelLayers()) {
             // TiledTileLayers are the layers where actual tiles are drawn
@@ -33,6 +35,13 @@ public final class MapRenderer {
                 // iterate through all the tiles on the layer
                 for (int x = 0; x < map.getWidth(); x++) {
                     for (int y = 0; y < map.getHeight(); y++) {
+                        TiledTile tile = tiledTileLayer.getTile(x, y);
+                        String tilesetName = tile.getTileset().getName();
+                        Image image = ImageCropper.cropImage(
+                                tile.getTileset().getPath(),
+                                x * map.getTileWidth(), y * map.getTileHeight(),
+                                map.getTileWidth(),
+                                map.getTileHeight());
 
                     }
                 }
