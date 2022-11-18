be64811367f5c69974d9a3e3e20c0c909dd96a65
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 20:32:47 -0800

    change to just accept a path does not start from resources anymore

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/ImageCropper.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/ImageCropper.java
@@ -17,7 +17,7 @@ public final class ImageCropper {
      * Crops and returns a new sub image from the original image.
      * Code used from stackoverflow post:
      * https://stackoverflow.com/questions/14802374/effective-image-cropping-in-javafx
-     * @param imgPath the path of the original image, starting at the root of the resources directory
+     * @param imgPath the path of the original image
      * @param x the x position of the desired sub image
      * @param y the y position of the desired sub image
      * @param width the width of the desired sub image
@@ -25,7 +25,7 @@ public final class ImageCropper {
      * @return the cropped image
      */
     public static WritableImage cropImage(final String imgPath, final int x, final int y, final int width, final int height) {
-        Image originalImage = new Image(HelloApplication.class.getResource(imgPath).getPath());
+        Image originalImage = new Image(imgPath);
         // retrieves the pixel reader of the original image
         PixelReader pixelReader = originalImage.getPixelReader();
         // returns a sub image at the specified region
