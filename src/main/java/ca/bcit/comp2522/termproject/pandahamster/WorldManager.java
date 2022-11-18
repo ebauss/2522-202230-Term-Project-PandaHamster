e681ac41e9e139bcf0e56fa7499a7120cae87f75
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-17 21:58:50 -0800

    add bodydef and fixturedef members

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/WorldManager.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/WorldManager.java
@@ -1,6 +1,9 @@
 package ca.bcit.comp2522.termproject.pandahamster;
 
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
 /**
@@ -10,6 +13,8 @@ import org.jbox2d.dynamics.World;
  */
 public final class WorldManager {
     private static World world;
+    private static BodyDef bodyDef;
+    private static FixtureDef fixtureDef;
     private WorldManager() { }
     /**
      * Returns the instance of the world. Subsequent calls will refer to the same world.
@@ -18,6 +23,8 @@ public final class WorldManager {
     public static World getInstance() {
         if (world == null) {
             world = new World(new Vec2(0, 0));
+            bodyDef = new BodyDef();
+            fixtureDef = new FixtureDef();
         }
         return world;
     }
