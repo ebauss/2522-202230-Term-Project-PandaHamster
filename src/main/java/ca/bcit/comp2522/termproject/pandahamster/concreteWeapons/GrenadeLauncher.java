dedb0e4ee0b487ba3f0088f92e1f897557d5f2be
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-27 09:33:12 -0800

    update GrenadeLauncher modify constant descriptions to match new name

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/concreteWeapons/GrenadeLauncher.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/concreteWeapons/GrenadeLauncher.java
@@ -3,45 +3,45 @@ package ca.bcit.comp2522.termproject.pandahamster.concreteWeapons;
 import ca.bcit.comp2522.termproject.pandahamster.AbstractWeapon;
 
 /**
- * Represents an object of type Grenade.
- * Grenades have high damage, medium range, hit multiple targets and slow fire rate.
+ * Represents an object of type GrenadeLauncher.
+ * Grenade launchers have high damage, medium range, hit multiple targets and slow fire rate.
  *
  * @author Evon Bausa & Alex Liu
  * @version 0.0
  */
 public class GrenadeLauncher extends AbstractWeapon {
     /**
-     * The grenade damage.
+     * The grenade launcher damage.
      */
     public static final float DAMAGE = 50f;
     /**
-     * The grenade attack speed. The time it takes to do a single attack in seconds.
+     * The grenade launchere attack speed. The time it takes to do a single attack in seconds.
      */
     public static final float ATTACK_SPEED = 1.5f;
     /**
-     * If the grenade can hit multiple enemies with a single attack.
+     * If the grenade launcher can hit multiple enemies with a single attack.
      */
     public static final boolean HAS_AOE = true;
     /**
-     * The grenade attack range in pixels.
+     * The grenade launcher attack range in pixels.
      */
-    public static final long ATTACK_RANGE = 75;
+    public static final long ATTACK_RANGE = 200;
     /**
-     * The grenade cost in the shop.
+     * The grenade launcher cost in the shop.
      */
     public static final long COST = 4500;
     /**
-     * The grenade ammo capacity, total ammo of the assault rifle.
+     * The grenade launcher ammo capacity, total ammo of the grenade launcher.
      */
     public static final long AMMO_CAPACITY = 80;
     /**
-     * The grenade clip size, how many shots can be fired before reloading is required.
+     * The grenade launcher clip size, how many shots can be fired before reloading is required.
      */
     public static final long CLIP_SIZE = 5;
     /**
      * A string representation of the name of the object.
      */
-    public static final String NAME = "Grenade";
+    public static final String NAME = "Grenade Launcher";
 
     /**
      * Constructs an object of type Grenade.
