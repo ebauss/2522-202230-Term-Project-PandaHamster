94e68f9907d62162591882205bddb7db558770f4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 14:56:44 -0800

    update Shotgun use fireSingleShot instead

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/concreteWeapons/Shotgun.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/concreteWeapons/Shotgun.java
@@ -78,11 +78,7 @@ public class Shotgun extends AbstractWeapon {
                 } else {
                     start = start.sub(new Vec2(0, bulletDistance));
                 }
-                Bullet bullet = new Bullet(playerPos.x, playerPos.y, ATTACK_RANGE, GameEntityType.Player);
-                bullet.setOrigin(new Vec2(playerPos.x, playerPos.y));
-                BulletManager.addBullets(bullet);
-                bullet.getBody().getFixtureList().m_isSensor = true;
-                applyVelocity(bullet, start, start);
+                fireSingleShot(ATTACK_RANGE, start, start);
             }
         }
     }
