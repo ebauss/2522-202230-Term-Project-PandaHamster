d365022e8221d3271364deed87a29898856a25dd
Evon Bausa <evon.bausa@gmail.com>
2022-12-03 19:23:20 -0800

    Revise base class

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/Base.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/Base.java
@@ -12,4 +12,25 @@ public class Base {
     private long health;
     // TODO Implement towers so that you can uncomment this.
 //    private ArrayList<AbstractTower> towerCollection;
+
+    /**
+     * Constructs an object of type Base.
+     */
+    public Base() {
+        this.health = 3000;
+    }
+
+    /**
+     * Reduces the base health;
+     */
+    public void reduceBaseHealth() {
+        this.health -= 100;
+    }
+
+    /**
+     * Resets the health of the base.
+     */
+    public void resetBaseHeatlh() {
+        this.health = 3000;
+     }
 }
