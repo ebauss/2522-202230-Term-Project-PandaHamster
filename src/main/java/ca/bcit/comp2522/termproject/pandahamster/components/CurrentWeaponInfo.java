aedd8b11b9873b986bce39564cbf8ad9edc3dc9c
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 17:38:03 -0800

    update CurrentWeaponInfo: change to grid pane

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
@@ -2,12 +2,17 @@ package ca.bcit.comp2522.termproject.pandahamster.components;
 
 import ca.bcit.comp2522.termproject.pandahamster.Player;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.StringBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 
 /**
  * UI element to display current weapon. current clip and ammo capacity.
@@ -17,20 +22,39 @@ import javafx.scene.control.Label;
 public final class CurrentWeaponInfo implements DynamicUi {
     private final StringProperty currentWeaponName = new SimpleStringProperty();
     private final DoubleProperty currentClipSize = new SimpleDoubleProperty();
+    private final DoubleProperty clipSize = new SimpleDoubleProperty();
     private final DoubleProperty currentAmmoCapacity = new SimpleDoubleProperty();
-    private final Label currentWeaponInfo = new Label();
+    private final GridPane currentWeaponInfo = new GridPane();
+    private final Label weaponName = new Label();
+    private final Label clip = new Label();
+    private final Label ammo = new Label();
 
     /**
      * Creates a new current weapon info label.
      */
     private CurrentWeaponInfo() {
-        currentWeaponInfo.getStyleClass().add("game-label");
-        StringBinding stringBinding = Bindings.createStringBinding(() -> String.format(
-                "%s\n%d/%d", currentWeaponName.get(),
-                        (int) currentClipSize.get(),
-                        (int) currentAmmoCapacity.get()),
-                currentWeaponName, currentClipSize, currentAmmoCapacity);
-        currentWeaponInfo.textProperty().bind(stringBinding);
+        weaponName.getStyleClass().add("game-label");
+        ammo.getStyleClass().add("game-label");
+        weaponName.textProperty().bind(currentWeaponName);
+        clip.textProperty().bind(Bindings.createStringBinding(() -> String.format(
+                "%d/%d", (int) currentClipSize.get(), (int) clipSize.get()
+        ), currentClipSize, clipSize));
+
+        ammo.textProperty().bind(Bindings.createStringBinding(() -> String.format(
+                "%d", (int) currentAmmoCapacity.get()
+        ), currentAmmoCapacity));
+        currentWeaponInfo.getStyleClass().add("grid");
+        ColumnConstraints column = new ColumnConstraints();
+        column.setHalignment(HPos.CENTER);
+        column.setPercentWidth(50);
+        column.setHgrow(Priority.ALWAYS);
+        RowConstraints row = new RowConstraints();
+        row.setValignment(VPos.CENTER);
+        currentWeaponInfo.getColumnConstraints().addAll(column, column);
+        currentWeaponInfo.getRowConstraints().addAll(row, row);
+        currentWeaponInfo.add(weaponName, 0, 0, 2, 1);
+        currentWeaponInfo.add(clip, 0, 1, 1, 1);
+        currentWeaponInfo.add(ammo, 1, 1, 1, 1);
     }
     /**
      * Changes info of the current weapon on the label when weapon is swapped.
@@ -39,6 +63,7 @@ public final class CurrentWeaponInfo implements DynamicUi {
     public void update() {
         currentWeaponName.setValue(Player.getInstance().getCurrentWeapon().getName());
         currentClipSize.setValue(Player.getInstance().getCurrentWeapon().getCurrentClipCount());
+        clipSize.setValue(Player.getInstance().getCurrentWeapon().getClipSize());
         currentAmmoCapacity.set(Player.getInstance().getCurrentWeapon().getAmmoCapacity());
     }
 
@@ -55,7 +80,7 @@ public final class CurrentWeaponInfo implements DynamicUi {
      * Returns the weapon info label that exists inside.
      * @return the weapon info label
      */
-    public Label getCurrentWeaponInfoLabel() {
+    public GridPane getCurrentWeaponInfoGrid() {
         return currentWeaponInfo;
     }
 }
