6d83ffccb7c8bc100e94f81edbf5b764615c8f7d
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 21:41:14 -0800

    add jackson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ca.bcit.comp2522.termproject.pandahamster {
     requires javafx.fxml;
     requires TiledReader;
     requires jbox2d.library;
+    requires com.fasterxml.jackson.core;
 
 
     opens ca.bcit.comp2522.termproject.pandahamster to javafx.fxml;
