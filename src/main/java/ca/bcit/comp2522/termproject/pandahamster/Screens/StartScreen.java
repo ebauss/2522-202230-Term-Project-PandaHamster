5dd170611f99c68a17ed7f8b0395ad255692d8bb
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 23:48:48 -0800

    update StartScreen call load when start button clicked

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/Screens/StartScreen.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/Screens/StartScreen.java
@@ -1,5 +1,6 @@
 package ca.bcit.comp2522.termproject.pandahamster.Screens;
 
+import ca.bcit.comp2522.termproject.pandahamster.GameSaveAndLoad;
 import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
@@ -11,6 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+
+import java.io.IOException;
 
 /**
  * Represents the start screen of the game.
@@ -53,6 +56,11 @@ public final class StartScreen extends Scene {
         ROOT.getChildren().add(layout);
         Button playButton = new Button("Start");
         playButton.setOnAction((value) -> {
+            try {
+                GameSaveAndLoad.load();
+            } catch (IOException e) {
+                System.out.println("Illegal IO operation");
+            }
             ScreenManager.changeScreen(ScreenManager.GAME_SCREEN);
         });
         Button quitButton = new Button("Quit");
