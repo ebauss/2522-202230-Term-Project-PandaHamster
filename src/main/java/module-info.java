aeca555466c635d16f33192700a72c08d6314e1a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 23:44:01 -0800

    add gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module ca.bcit.comp2522.termproject.pandahamster {
     requires TiledReader;
     requires jbox2d.library;
     requires com.fasterxml.jackson.databind;
-
+    requires com.google.gson;
 
     opens ca.bcit.comp2522.termproject.pandahamster to javafx.fxml;
     exports ca.bcit.comp2522.termproject.pandahamster;
