352be86abbe0cedd57e636ec4c010ca5e230cf04
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 18:29:15 -0800

    update GameMap: add map width and height and getters and setters for them

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/GameMap.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/GameMap.java
@@ -1,2 +1,23 @@
-package ca.bcit.comp2522.termproject.pandahamster;public class GameMap {
+package ca.bcit.comp2522.termproject.pandahamster;
+
+public final class GameMap {
+    private static int MAP_HEIGHT;
+    private static int MAP_WIDTH;
+    private GameMap() { }
+
+    public static int getMapHeight() {
+        return MAP_HEIGHT;
+    }
+
+    public static void setMapHeight(int mapHeight) {
+        MAP_HEIGHT = mapHeight;
+    }
+
+    public static int getMapWidth() {
+        return MAP_WIDTH;
+    }
+
+    public static void setMapWidth(int mapWidth) {
+        MAP_WIDTH = mapWidth;
+    }
 }
