48ef780066e313358abf35ad1fce18f12b01325f
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-04 12:46:05 -0800

    make ammo infinite

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
@@ -23,38 +23,29 @@ public final class CurrentWeaponInfo implements DynamicUi {
     private final StringProperty currentWeaponName = new SimpleStringProperty();
     private final DoubleProperty currentClipSize = new SimpleDoubleProperty();
     private final DoubleProperty clipSize = new SimpleDoubleProperty();
-    private final DoubleProperty currentAmmoCapacity = new SimpleDoubleProperty();
     private final GridPane currentWeaponInfo = new GridPane();
     private final Label weaponName = new Label();
     private final Label clip = new Label();
-    private final Label ammo = new Label();
 
     /**
      * Creates a new current weapon info label.
      */
     private CurrentWeaponInfo() {
         weaponName.getStyleClass().add("game-label");
-        ammo.getStyleClass().add("game-label");
         weaponName.textProperty().bind(currentWeaponName);
         clip.textProperty().bind(Bindings.createStringBinding(() -> String.format(
                 "%d/%d", (int) currentClipSize.get(), (int) clipSize.get()
         ), currentClipSize, clipSize));
-
-        ammo.textProperty().bind(Bindings.createStringBinding(() -> String.format(
-                "%d", (int) currentAmmoCapacity.get()
-        ), currentAmmoCapacity));
         currentWeaponInfo.getStyleClass().add("grid");
         ColumnConstraints column = new ColumnConstraints();
         column.setHalignment(HPos.CENTER);
-        column.setPercentWidth(50);
         column.setHgrow(Priority.ALWAYS);
         RowConstraints row = new RowConstraints();
         row.setValignment(VPos.CENTER);
-        currentWeaponInfo.getColumnConstraints().addAll(column, column);
+        currentWeaponInfo.getColumnConstraints().addAll(column);
         currentWeaponInfo.getRowConstraints().addAll(row, row);
-        currentWeaponInfo.add(weaponName, 0, 0, 2, 1);
-        currentWeaponInfo.add(clip, 0, 1, 1, 1);
-        currentWeaponInfo.add(ammo, 1, 1, 1, 1);
+        currentWeaponInfo.add(weaponName, 0, 0);
+        currentWeaponInfo.add(clip, 0, 1);
     }
     /**
      * Changes info of the current weapon on the label when weapon is swapped.
@@ -64,7 +55,6 @@ public final class CurrentWeaponInfo implements DynamicUi {
         currentWeaponName.setValue(Player.getInstance().getCurrentWeapon().getName());
         currentClipSize.setValue(Player.getInstance().getCurrentWeapon().getCurrentClipCount());
         clipSize.setValue(Player.getInstance().getCurrentWeapon().getClipSize());
-        currentAmmoCapacity.set(Player.getInstance().getCurrentWeapon().getAmmoCapacity());
     }
 
     /**
