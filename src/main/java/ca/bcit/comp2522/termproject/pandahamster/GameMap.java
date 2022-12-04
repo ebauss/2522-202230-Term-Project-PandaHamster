1a0820fa5ca344fc62f257696d6f7599b3ddcbe5
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 23:46:30 -0800

    update GameMap add static initialization block to set map width and height

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/GameMap.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/GameMap.java
@@ -1,8 +1,18 @@
 package ca.bcit.comp2522.termproject.pandahamster;
 
+import org.tiledreader.FileSystemTiledReader;
+import org.tiledreader.TiledMap;
+import org.tiledreader.TiledReader;
+
 public final class GameMap {
     private static int MAP_HEIGHT;
     private static int MAP_WIDTH;
+    static {
+        TiledReader reader = new FileSystemTiledReader();
+        TiledMap map = reader.getMap(PandaHamster.class.getResource("/gameMap.tmx").getPath());
+        MAP_HEIGHT = map.getHeight() * map.getTileHeight();
+        MAP_WIDTH = map.getWidth() * map.getTileWidth();
+    }
     private GameMap() { }
 
     public static int getMapHeight() {
