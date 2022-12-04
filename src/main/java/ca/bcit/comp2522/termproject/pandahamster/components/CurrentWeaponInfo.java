6a7b05c0ac4e68bc139dae4f42898fe1ac04831a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-12-03 16:00:10 -0800

    update CurrentWeaponInfo: add extra bindings to display current clip and ammo

--- src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
+++ src/main/java/ca/bcit/comp2522/termproject/pandahamster/components/CurrentWeaponInfo.java
@@ -1,6 +1,10 @@
 package ca.bcit.comp2522.termproject.pandahamster.components;
 
 import ca.bcit.comp2522.termproject.pandahamster.Player;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.control.Label;
@@ -12,13 +16,20 @@ import javafx.scene.control.Label;
  */
 public final class CurrentWeaponInfo implements DynamicUi {
     private final StringProperty currentWeaponName = new SimpleStringProperty();
+    private final DoubleProperty currentClipSize = new SimpleDoubleProperty();
+    private final DoubleProperty currentAmmoCapacity = new SimpleDoubleProperty();
     private final Label currentWeaponInfo = new Label();
 
     /**
      * Creates a new current weapon info label.
      */
     private CurrentWeaponInfo() {
-        currentWeaponInfo.textProperty().bind(currentWeaponName);
+        StringBinding stringBinding = Bindings.createStringBinding(() -> String.format(
+                "%s\n%d/%d", currentWeaponName.get(),
+                        (int) currentClipSize.get(),
+                        (int) currentAmmoCapacity.get()),
+                currentWeaponName, currentClipSize, currentAmmoCapacity);
+        currentWeaponInfo.textProperty().bind(stringBinding);
     }
     /**
      * Changes info of the current weapon on the label when weapon is swapped.
@@ -26,6 +37,8 @@ public final class CurrentWeaponInfo implements DynamicUi {
     @Override
     public void update() {
         currentWeaponName.setValue(Player.getInstance().getCurrentWeapon().getName());
+        currentClipSize.setValue(Player.getInstance().getCurrentWeapon().getCurrentClipCount());
+        currentAmmoCapacity.set(Player.getInstance().getCurrentWeapon().getAmmoCapacity());
     }
 
     /**
